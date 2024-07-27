3c978f37e006b319f6a217bbf4bbc43810635f95
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-07-27 11:46:05 -0400

    jsonPath

--- src/main/java/io/techleadacademy/stepDefs/apiSteps/rawTests/_006_GetData.java
+++ src/main/java/io/techleadacademy/stepDefs/apiSteps/rawTests/_006_GetData.java
@@ -1,11 +1,17 @@
 package io.techleadacademy.stepDefs.apiSteps.rawTests;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 
 
 public class _006_GetData {
     String endpointUser = "https://jsonplaceholder.typicode.com/users/1";
+    String endpointUsers = "https://jsonplaceholder.typicode.com/users";
 
     @Test
     public void test01(){
@@ -47,4 +53,83 @@ public class _006_GetData {
 
         System.out.println("===================>>" + str);
     }
+
+    @Test
+    public void test04(){
+        //Task
+        String str = given()
+                .get("https://gorest.co.in/public/v1/users/7167427")
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("data.email");
+
+        System.out.println("===================>>" + str);
+    }
+
+    @Test
+    public void test05(){
+        //Get list of items of all objects(user)
+        List<String> namesList = given()
+                .get(endpointUsers)
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("name");
+
+//        for(String each: namesList){
+//            System.out.println(each);
+//        }
+
+//        namesList.forEach(each -> System.out.println(each));
+
+        namesList.forEach(System.out::println);
+    }
+
+    //JsonPath
+    @Test
+    public void test06(){
+        //Get id of the user and store in an int
+        int id = given()
+                .get(endpointUser)
+                .jsonPath()
+                        .getInt("id");
+
+        System.out.println("===================>>" + id);
+    }
+
+    @Test
+    public void test07(){
+        Response response = given()
+                .get(endpointUser);
+
+        JsonPath jsonPath = response.jsonPath();
+
+        System.out.println("===================>>" + jsonPath.get("name"));
+        System.out.println("===================>>" + jsonPath.get("company.name"));
+    }
+
+    @Test
+    public void test08(){
+        //Get id of the user and store in an int
+        String str = given()
+                .get("https://jsonplaceholder.typicode.com/users")
+                .jsonPath()
+                .getString("company.name[2]");
+
+        System.out.println("===================>>" + str);
+    }
+
+    @Test
+    public void test09(){
+        //Get id of the user and store in an int
+        List<String> str = given()
+                .get("https://jsonplaceholder.typicode.com/users")
+                .jsonPath()
+                .getList("name");
+
+        str.forEach(System.out::println);
+    }
+
+
 }
