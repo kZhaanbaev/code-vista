28cdc17164b5418786ecff5b2c6316887484b245
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-07-28 11:53:23 -0400

    put and delete requests

--- src/main/java/io/techleadacademy/stepDefs/apiSteps/rawTests/_007_POST.java
+++ src/main/java/io/techleadacademy/stepDefs/apiSteps/rawTests/_007_POST.java
@@ -1,9 +1,11 @@
 package io.techleadacademy.stepDefs.apiSteps.rawTests;
 
 import io.restassured.http.ContentType;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import static io.restassured.RestAssured.*;
 
@@ -58,4 +60,80 @@ public class _007_POST {
                 .body();
 
     }
+
+    //Task
+    @Test
+    public void test03(){
+        String token = "d8d4c1540d64c51e123c7383dc59d405daea142f14a8cf29fb5a349a3970b104";
+        Map<String, Object> data = new HashMap<>();
+        data.put("user_id", 7204156);
+        data.put("title", "Test post");
+        data.put("body", "Post body");
+
+        int id = given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+                .body(data)
+
+                .when()
+                .post("https://gorest.co.in/public/v2/posts")
+                .jsonPath()
+                .getInt("id");
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .get("https://gorest.co.in/public/v2/posts/" + id)
+                .then()
+                .statusCode(200)
+                .log()
+                .body();
+
+    }
+
+    //Task
+    @Test
+    public void test04(){
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0bGEubGl2ZXByb2plY3QudGVhbTJAZ21haWwuY29tIiwiaWF0IjoxNzIyMTc3MTE1LCJleHAiOjE3MjIyNjM1MTUsImZpcnN0TmFtZSI6IkpvaG4iLCJsYXN0TmFtZSI6IlNtaXRoIn0.L1SpCOmrKrfMVitRGmTPeNNO3T20NvkdMe0334fMPUM";
+        String data = "{\n" +
+                "  \"userId\": 96,\n" +
+                "  \"taskId\": 2003,\n" +
+                "  \"taskName\": \"Task91\",\n" +
+                "  \"instruction\": \"Test instruction\",\n" +
+                "  \"code\": \"some code\",\n" +
+                "  \"status\": \"new\",\n" +
+                "  \"moduleName\": \"Exceptions\"\n" +
+                "}";
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+                .body(data)
+                .when()
+                .post("http://api.code-vista.net/api/submission")
+                .then()
+                .statusCode(201)
+                .log()
+                .body();
+
+//        given()
+//                .header("Authorization", "Bearer " + token)
+//                .queryParam("userId", 96)
+//                .queryParam("taskName", "Task91")
+//                .get("http://api.code-vista.net/api/submission/by-user-and-task")
+//                .then()
+//                .statusCode(200)
+//                .log()
+//                .body();
+
+        List<String> taskNames = given()
+                .header("Authorization", "Bearer " + token)
+                .get("http://api.code-vista.net/api/submission/by-user/96")
+                .jsonPath()
+                .getList("taskName");
+
+        System.out.println(taskNames);
+        Assert.assertTrue(taskNames.contains("Task90"));
+    }
+
+
 }
