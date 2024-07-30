e265a21f5aa26c728abf0c02cc880704bae7b06f
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-07-30 18:30:53 -0400

    api

---
+++ src/main/java/io/techleadacademy/utils/ApiUtils.java
@@ -0,0 +1,13 @@
+package io.techleadacademy.utils;
+
+import io.techleadacademy.core.TestContext;
+
+public class ApiUtils {
+    private TestContext testContext;
+
+    public ApiUtils(TestContext testContext){
+        this.testContext = testContext;
+    }
+
+
+}
