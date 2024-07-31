4f0d6ba2b7c1e10a8bac1226ce7d95a7b181e257
Marianna Antonian <mariannahaney@gmail.com>
2024-07-31 19:35:07 -0400

    overloaded createNewModuleMethod

--- src/main/java/io/techleadacademy/utils/ApiUtils.java
+++ src/main/java/io/techleadacademy/utils/ApiUtils.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.techleadacademy.core.TestContext;
 import java.util.Map;
+import io.techleadacademy.pojo.Module;
 
 
 public class ApiUtils {
@@ -41,6 +42,20 @@ public class ApiUtils {
         testContext.API().requestSpecification.queryParam("moduleName", moduleData.get("moduleName"))
                 .queryParam("videoLink", moduleData.get("videoLink"))
                 .queryParam("moduleOrder", moduleData.get("moduleOrder"));
+
+        testContext.API().response = testContext.API().requestSpecification
+                .request(Method.POST, "http://api.code-vista.net/api/modules");
+    }
+
+    public void createNewModule(Module module) {
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYXJpYW5uYS5hbnRvbmlhbkBnbWF" +
+                "pbC5jb20iLCJpYXQiOjE3MjIzODkwNzQsImV4cCI6MTcyMjQ3NTQ3NCwiZmlyc3ROYW1lIjoiTWFyaWF" +
+                "ubmEiLCJsYXN0TmFtZSI6IkFudG9uaWFuIn0.eYLKKf5vmpnUA7TBSsmx_lJ75DZaVi3Lr-d1PAYPEME";
+        testContext.API().requestSpecification = RestAssured.given();
+        testContext.API().requestSpecification.auth().oauth2(token);
+        testContext.API().requestSpecification.queryParam("moduleName", module.getModuleName())
+                .queryParam("videoLink", module.getVideoLink())
+                .queryParam("moduleOrder", module.getModuleOrder());
 
         testContext.API().response = testContext.API().requestSpecification
                 .request(Method.POST, "http://api.code-vista.net/api/modules");
