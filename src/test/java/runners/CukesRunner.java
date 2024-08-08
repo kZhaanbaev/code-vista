67977a1aea49e761d24e3bb57563fa79458e022c
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-08-08 01:40:43 -0400

    small changes for jenkins test

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "io.techleadacademy.stepDefs",
         stepNotifications = true,
-        tags = "@users-table",
+        tags = "@US100",
         dryRun = true
 )
 public class CukesRunner {
