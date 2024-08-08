c858a52a0afe3728a4e35cc9689d0d2dcca2d0c6
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-08-08 11:11:16 -0400

    small changes for jenkins test

--- src/main/java/io/techleadacademy/utils/ConfigReader.java
+++ src/main/java/io/techleadacademy/utils/ConfigReader.java
@@ -7,37 +7,12 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-//    public static String readProperty(String property) {
-//        Properties prop;
-//        FileInputStream fileInput;
-//
-//        try {
-//            fileInput = new FileInputStream("config-local.properties");
-//            prop = new Properties();
-//            prop.load(fileInput);
-//
-//            if (prop.getProperty(property) == null){
-//                try {
-//                    fileInput = new FileInputStream("config.properties");
-//                    prop = new Properties();
-//                    prop.load(fileInput);
-//                } catch (IOException ex) {
-//                    throw new RuntimeException(ex);
-//                }
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        return prop.getProperty(property);
-//    }
-
     public static String readProperty(String property) {
         switch (property) {
             case "username-student":
-                return System.getenv("STUDENT_USR") != null ? System.getenv("STUDENT_USR") : new ConfigReader().getPropertyFromFile(property);
+                return System.getenv("STUDENT_USR");// != null ? System.getenv("STUDENT_USR") : new ConfigReader().getPropertyFromFile(property);
             case "password-student":
-                return System.getenv("STUDENT_PSW") != null ? System.getenv("STUDENT_PSW") : new ConfigReader().getPropertyFromFile(property);
+                return System.getenv("STUDENT_PSW");// != null ? System.getenv("STUDENT_PSW") : new ConfigReader().getPropertyFromFile(property);
             default:
                 return new ConfigReader().getPropertyFromFile(property);
         }
@@ -67,6 +42,4 @@ public class ConfigReader {
 
         return prop.getProperty(property);
     }
-
-
 }
