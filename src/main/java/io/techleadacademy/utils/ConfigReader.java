2c8e1c365e0f70fe501e7498ed043c475e05214e
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-08-08 11:30:37 -0400

    small changes for jenkins test

--- src/main/java/io/techleadacademy/utils/ConfigReader.java
+++ src/main/java/io/techleadacademy/utils/ConfigReader.java
@@ -1,23 +1,18 @@
 package io.techleadacademy.utils;
 
-import org.junit.Assert;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
     public static String readProperty(String property) {
-        System.out.println("===============>>>>>>>>>: " + property);
-        System.out.println("-------------->>>>>>>>>>: " + System.getenv("STUDENT_USR"));
-        switch (property) {
-            case "username-student":
-                return System.getenv("STUDENT_USR");// != null ? System.getenv("STUDENT_USR") : new ConfigReader().getPropertyFromFile(property);
-            case "password-student":
-                return System.getenv("STUDENT_PSW");// != null ? System.getenv("STUDENT_PSW") : new ConfigReader().getPropertyFromFile(property);
-            default:
-                return "123";//new ConfigReader().getPropertyFromFile(property);
-        }
+        return switch (property) {
+            case "username-student" ->
+                    System.getenv("STUDENT_USR") != null ? System.getenv("STUDENT_USR") : new ConfigReader().getPropertyFromFile(property);
+            case "password-student" ->
+                    System.getenv("STUDENT_PSW") != null ? System.getenv("STUDENT_PSW") : new ConfigReader().getPropertyFromFile(property);
+            default -> new ConfigReader().getPropertyFromFile(property);
+        };
     }
 
     private String getPropertyFromFile(String property) {
@@ -25,13 +20,13 @@ public class ConfigReader {
         FileInputStream fileInput;
 
         try {
-            fileInput = new FileInputStream("config-local.properties");
+            fileInput = new FileInputStream("config.properties");
             prop = new Properties();
             prop.load(fileInput);
 
             if (prop.getProperty(property) == null){
                 try {
-                    fileInput = new FileInputStream("config.properties");
+                    fileInput = new FileInputStream("config-local.properties");
                     prop = new Properties();
                     prop.load(fileInput);
                 } catch (IOException ex) {
