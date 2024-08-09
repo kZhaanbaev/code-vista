d397d9149675dd5a40b54279e52a9ca712edd5d7
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2024-08-08 21:34:45 -0400

    added saucelabs configs

--- src/main/java/io/techleadacademy/core/Driver.java
+++ src/main/java/io/techleadacademy/core/Driver.java
@@ -1,16 +1,27 @@
 package io.techleadacademy.core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.techleadacademy.utils.ConfigReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Driver {
-
     public WebDriver initializeDriver(String browserType){
+        if (!ConfigReader.readProperty("runInSauceLabs").equals("true"))
+            return getWebDriver("chrome");
+        return getRemoteDriver();
+    }
+
+    private WebDriver getWebDriver(String browserType) {
         WebDriver driver = null;
         switch (browserType.toLowerCase()){
             case "chrome":
@@ -27,8 +38,32 @@ public class Driver {
                 System.out.println("Wrong browser type");
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//        driver.manage().window().maximize();
         return driver;
     }
+
+    private WebDriver getRemoteDriver(){
+        ChromeOptions options = new ChromeOptions();
+        options.setPlatformName("Windows 11");
+        options.setBrowserVersion("latest");
+
+        Map<String, Object> sauceOptions = new HashMap<>();
+        sauceOptions.put("username", ConfigReader.readProperty("SAUCE_USERNAME"));
+        sauceOptions.put("accessKey", ConfigReader.readProperty("SAUCE_ACCESS_KEY"));
+
+        //Update this to see if your test was ran in SauceLabs
+        sauceOptions.put("name", "Scenario execution - Test");
+
+        options.setCapability("sauce:options", sauceOptions);
+
+        URL url = null;
+        try {
+            url = new URL("https://ondemand.saucelabs.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new RemoteWebDriver(url, options);
+    }
+
 
 }
